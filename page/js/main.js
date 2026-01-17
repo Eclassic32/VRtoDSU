163b96bddc7ed78fdd652b0adc5d18448ed39832
Eclassic32 <eclassic32004@gmail.com>
2026-01-17 14:34:27 +0500

    bruh who deleted xr.js

--- page/js/main.js
+++ page/js/main.js
@@ -1,5 +1,6 @@
 import {HeadsetInfo, LeftControllerInfo, RightControllerInfo} from './class.js';
 import {HeadsetInfoElements, LeftControllerInfoElements, RightControllerInfoElements} from './elemets.js';
+import "./xr.js";
 
 // Data models for storing device info (can be used for further processing)
 const headsetInfo = new HeadsetInfo();
